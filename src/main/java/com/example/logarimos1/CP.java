c1e0a1b3967bd2eb092df57bd9fb91f3da294773
Evelyn Avila <evelynavila49@gmail.com>
2024-05-02 14:37:12 -0400

    Avanzando en CP
    Creando el algoritmo CP

--- src/main/java/com/example/logarimos1/CP.java
+++ src/main/java/com/example/logarimos1/CP.java
@@ -1,9 +1,12 @@
 package com.example.logarimos1;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+import java.lang.Math;
 
 /**
- *
+ * Clase que conforma el método Ciaccia-Patella.
  */
 public class CP {
   //Hay que definir bien estos valores
@@ -15,8 +18,143 @@ public class CP {
     this.b = 0.5*tamanoB;
   }
 
-  public ArrayList<Node> cp(ArrayList<Pair> cIn) {
-    return null;
+  /**
+   * Función que busca el par más cercano al punto p de una lista de puntos.
+   * @param p punto al que se le busca el par más cercano
+   * @param sample lista de puntos candidatos
+   * @return El índice del par más cercano encontrado.
+   */
+  public Integer nearestSample(Pair p, ArrayList<Pair> sample) {
+    int n = sample.size();
+    double minDist = Double.MAX_VALUE;
+    int resultIndex = 0;
+
+    for(int i = 0; i<n;i++) {
+      Pair sample_i = sample.get(i);
+
+      if(p.dist(sample_i)<minDist) {
+        minDist = p.dist(sample_i);
+        resultIndex = i;
+      }
+    }
+
+    return resultIndex;
+  }
+
+  /**
+   * Método que asigna un punto a un sample.
+   * @param p el punto a reasignar
+   * @param listSamples la lista de samples disponibles
+   * @param hash el hash que guarda la información de cada sample y el conjunto de puntos asociado
+   */
+  public void asignSample(Pair p, ArrayList<Pair> listSamples, HashMap<Integer, ArrayList<Pair>> hash) {
+    //Se consigue el sample más cercano al punto p
+    int index = nearestSample(p, listSamples);
+
+    //Se obtiene la lista de puntos de ese sample y se actualiza después de agregar el punto p
+    ArrayList<Pair> points = hash.get(index);
+
+    if (points == null) {
+      points = new ArrayList<>();
+    }
+
+    points.add(p);
+    hash.put(index, points);
+  }
+
+  /**
+   * Utiliza el algoritmo Ciaccia-Patella para construir un M-Tree.
+   * @param cIn Es un conjunto de puntos (Pares).
+   * @return El M-Tree construido
+   */
+  public NodeCP cp(ArrayList<Pair> cIn) {
+    //Se revisa si los puntos de cIn se pueden contener en un bloque o no, en caso de hacerlo retornamos
+    if (cIn.size() <= B) {
+      NodeCP a = new NodeCP();
+      a.insert(cIn);
+      return a;
+    } else {
+      //Definimos una variable random
+      Random random = new Random();
+      random.nextInt();
+
+      int n = cIn.size();
+
+      //Conjunto que contendrá a los samples de puntos
+      ArrayList<Pair> f = new ArrayList<>();
+
+      // F contiene los samples
+      // samples contiene el key: indice y value: sus puntos asociados
+      HashMap<Integer,ArrayList<Pair>> samples = new HashMap<>();
+
+      //Hacemos do-while en caso de tener que repetir la selección de samples.
+      do {
+        //k corresponde a la cantidad de puntos aleatorios a escoger para conformar la lista de samples
+        int k = (int) Math.min(B, n/B);
+        ArrayList<Integer> outIndex = new ArrayList<>();
+
+        //Se hace la selección de los índices de puntos que serán asigandos como samples
+        //en un ciclo hasta haber escogido los k valores necesarios
+        while (outIndex.size() < k) {
+          int j = random.nextInt(k);
+
+          //Se revisa que los puntos a insertar no sean repetidos
+          if(!outIndex.contains(j)) {
+            outIndex.add(j);
+            f.add(cIn.get(j));
+          }
+        }
+
+        //Asignamos un sample a cada punto de cIn
+        for (int i=0; i<n; i++) {
+          asignSample(cIn.get(i),f,samples);
+        }
+
+        //Revisamos si hay algun sample de tamaño menor a b
+        for (int i=0; i<f.size(); i++) {
+          ArrayList<Pair> points = samples.get(i);
+
+          //En caso de cumplir la condicion, el sample se vuelve un punto corriente y se reasigna junto con sus hijos.
+          if (points.size() < b) {
+            samples.remove(i);
+            Pair removedPoint = f.remove(i);
+
+            //Este sample pasa a ser un punto 'normal' por lo que lo agregamos a la lista de puntos por reasignar
+            //después de eliminarlo de la lista de samples
+            points.add(removedPoint);
+
+            // Cada punto del sample eliminado es reasignado al siguiente más cercano
+            for (Pair point : points) {
+              asignSample(point,f,samples);
+            }
+          }
+        }
+        //En caso de que el tamaño de F sea 1, repetimos el ciclo
+      } while (f.size() == 1);
+
+      //Creamos una lista para ir almacenando los árboles entregados por cp
+      ArrayList<NodeCP> trees = new ArrayList<>();
+
+      for(int i = 0; i < f.size(); i++) {
+        NodeCP a = cp(samples.get(i));
+
+        if (a.size() < b) {
+          Pair pfj = f.remove(i);
+
+          ArrayList<NodeCP> childs = a.getChilds();
+          for(int j=0; j<childs.size(); j++){
+            NodeCP child = childs.get(j);
+            f.add(child.getSample());
+            trees.add(child);
+          }
+        }
+
+        trees.add(a);
+      }
+
+
+
+    }
   }
 }
 
---
+++ src/main/java/com/example/logarimos1/NodeCP.java
@@ -0,0 +1,87 @@
+package com.example.logarimos1;
+
+import java.util.ArrayList;
+
+public class NodeCP {
+  /** Radio Cobertor */
+  private double r = 0;
+  /** Altura del árbol */
+  private double h = 0;
+  private Pair sample = null;
+
+  public NodeCP() {
+  }
+
+  /**
+   * @return radio cobertor
+   */
+  public double getR() {
+    return r;
+  }
+
+  /**
+   * @return altura del árbol
+   */
+  public double getH() {
+    return h;
+  }
+
+  /**
+   * @return El sample del Nodo asociado.
+   */
+  public Pair getSample() {
+    return sample;
+  }
+
+  /**
+   * Cambia el radio a newR
+   * @param newR nuevo radio cobertor
+   */
+  public void setR(double newR) {
+    r = newR;
+  }
+
+  /**
+   * Cambia la altura a newH
+   * @param newH la nueva altura del arbol
+   */
+  public void setH(double newH) {
+    h = newH;
+  }
+
+  /**
+   * Cambia el sample asociado al Nodo actual.
+   * @param newSample El nuevo sample asociado.
+   */
+  public void setSample(Pair newSample) {
+    sample = newSample;
+  }
+
+  /**
+   * Inserta una lista de puntos al nodo.
+   * @param cIn Lista de puntos (Pares).
+   */
+  public void insert(ArrayList<Pair> cIn) {
+
+  }
+
+  /**
+   * Busca un nodo específico dentro de un M-Tree.
+   * @return -1 si se encuentra, el número de ingresos a disco si lo encuentra.
+   */
+  public int search(NodeCP mTree, Pair query) {
+    // Comparar ingresos a disco
+    // Buscar el nodo correspondiente
+    // Cada vez que se ingresa a un nodo se suma 1
+
+    return null;
+  }
+
+  public double size() {
+
+  }
+
+  public ArrayList<NodeCP> getChilds() {
+
+  }
+}
