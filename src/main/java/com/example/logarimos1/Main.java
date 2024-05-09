fa89cf5bb3acdfd4b514832e742eee5c848fdc4a
NotBenja <benjamin.reyes.b@ug.uchile.cl>
2024-05-09 00:49:51 -0400

    Corrección Desv. Estándar

--- src/main/java/com/example/logarimos1/Main.java
+++ src/main/java/com/example/logarimos1/Main.java
@@ -90,30 +90,33 @@ public class Main {
  * @return El promedio de accesos a disco
  * */
   public static double hundredQueries(Node a, String filename) {
+    //Variable para guardar la suma total de accesos
     int sum = 0;
+    //Lista que contiene todos los accesos
     ArrayList<Integer> accesos = new ArrayList<>();
     for (int i = 0; i < querys.size(); i++) {
       Result r = a.search(querys.get(i), 0.02);
-      ArrayList<Pair> points = r.getPoints();
       int memoryAccess = r.getAccessCount();
       accesos.add(memoryAccess);
-      sum+=memoryAccess;
+      sum += memoryAccess;
       write.write(filename, "Query " + Integer.toString(i));
       write.write(filename, "Accesos: "+Integer.toString(memoryAccess));
       write.write(filename, "+++++++++++++++++++++++++++++++++++++++++");
     }
     //Variable que almacena la media de accesos a disco
-    int avg = sum/100;
+    int avg = sum/accesos.size();
     write.write(filename,"Accesos Promedio: " + avg);
     write.write(filename, "----------------------------------");
-    //Variable que almacenará la desviación estándar de accesos a disco
     double res_ds = 0;
 
-    for(int i=0; i<accesos.size(); i++) {
-      res_ds =+ Math.pow(accesos.get(i)-avg, 2);
+    for (int i = 0; i < accesos.size(); i++) {
+      res_ds += Math.pow(accesos.get(i) - avg, 2);
     }
-    res_ds = res_ds/100;
-    write.write(filename,"Desviación estándar: " + res_ds);
+    double varianza = res_ds / accesos.size();
+    double desviacionEstandar = Math.sqrt(varianza);
+    write.write(filename,"Desviación estándar: " + Double.toString(desviacionEstandar));
+    write.write(filename, "----------------------------------");
+    write.write(filename,"Varianza: " + Double.toString(varianza));
     write.write(filename, "----------------------------------");
     
     return res_ds;
@@ -127,36 +130,46 @@ public class Main {
     //ALGORITMO SS TESTEO
 
     //Generamos los inputs, que se guardan en el hash map inputs
-    generateTestingPairs(15);
 
+    generateTestingPairs(15);
+    System.out.println("Pares de testeo listos");
     //Leemos el listado de querys del archivo 'querys.txt' para obtener los 100 puntos de testeo, se guardarán
-    //en la variable
+    //en la variable querys
     reader.read("querys.txt", querys);
+    System.out.println("Querys listas");
 
     /**
-    //PARTE 1 Zamy
-    //Generamos los árboles de largo de inputs 2^10 y 2^11
-    createTreeSS(10);
-    createTreeSS(11);
+   //PARTE 1 Zamy
+   //Generamos los árboles de largo de inputs 2^10 y 2^11
+   createTreeSS(10);
+   createTreeSS(11);
 
-    //PARTE 2 Benja
-    //Generamos los árboles de largo de inputs 2^12 y 2^13
-    createTreeSS(12);
-    createTreeSS(13);
+   //PARTE 2 Benja
+   //Generamos los árboles de largo de inputs 2^12 y 2^13
+   System.out.println("Inicio");
+   createTreeSS(12);
+   System.out.println("Primer arbol listo");
+   createTreeSS(13);
+   System.out.println("Segundo arbol listo");
 
-    //PARTE 3 Eve
-    //Generamos los árboles de largo de inputs 2^14 y 2^15
-    createTreeSS(14);
-    createTreeSS(15);
+   //PARTE 3 Eve
+   //Generamos los árboles de largo de inputs 2^14 y 2^15
+   createTreeSS(14);
+   createTreeSS(15);
+
     */
 
+
     //Hacemos las 100 queries para los árboles correspondientes, ubicados en el array arbolesSS
+
     NodeSS a1 = arbolesSS.get(0);
     NodeSS a2 = arbolesSS.get(1);
 
+    System.out.println("Inicio Querys");
     double dsA1 = hundredQueries(a1, "resultsSS1.txt");
+    System.out.println("Query Arbol 1 lista");
     double dsA2 = hundredQueries(a2, "resultsSS2.txt");
-
+    System.out.println("Query Arbol 2 lista");
     // Desviación Estandar Sum((x-avgX)^2)/n, xAvg= promedio de la query, x = un punto de la query, n = 100
   }
 }
